f50257d28574269c8a2aa64f6891e59e57c72252
emarishkin <ee.marishkin@yandex.ru>
2024-10-03 10:09:14 +0400

    eee

--- Задачки js/zadacha.js
+++ Задачки js/zadacha.js
@@ -1,71 +1,73 @@
-function showMessage() {
-    alert( 'Всем привет!' );
-  }
+console.log("пвапвапе")
+// function showMessage() {
+//     alert( 'Всем привет!' );
+//   }
   
-  showMessage();
+//   showMessage();
 
 
-  function sum(a, b) {
-    return a + b;
-  }
+//   function sum(a, b) {
+//     return a + b;
+//   }
   
-  let result = sum(1, 2);
-  alert( result ); // 3
+//   let result = sum(1, 2);
+//   alert( result ); // 3
 
-  function checkAge(age) {
-    if (age >= 18) {
-      return true;
-    } else {
-      return confirm('А родители разрешили?');
-    }
-  }
+//   function checkAge(age) {
+//     if (age >= 18) {
+//       return true;
+//     } else {
+//       return confirm('А родители разрешили?');
+//     }
+//   }
   
-  let age = prompt('Сколько вам лет?', 18);
-  if ( checkAge(age) ) {
-    alert( 'Доступ получен' );
-  }
-    showMessage()     // показывает сообщение
-getAge()          // возвращает возраст (получая его каким-то образом)
-calcSum()         // вычисляет сумму и возвращает результат
-createForm()      // создаёт форму (и обычно возвращает её)
-checkPermission() // проверяет доступ, возвращая true/false
+//   let age = prompt('Сколько вам лет?', 18);
+//   if ( checkAge(age) ) {
+//     alert( 'Доступ получен' );
+//   }
+//     showMessage()     // показывает сообщение
+// getAge()          // возвращает возраст (получая его каким-то образом)
+// calcSum()         // вычисляет сумму и возвращает результат
+// createForm()      // создаёт форму (и обычно возвращает её)
+// checkPermission() // проверяет доступ, возвращая true/false
 
 
-function checkAge(age) {
-    if (age > 18) {
-      return true;
-    } else {
-      return confirm('Родители разрешили?');
-    }
-  }
-  checkAge(20)
+// function checkAge(age) {
+//     if (age > 18) {
+//       return true;
+//     } else {
+//       return confirm('Родители разрешили?');
+//     }
+//   }
+//   checkAge(20)
 
 
-  function min(a, b) {
-    if (a < b) {
-      return a;
-    } else {
-      return b;
-    }
-  }
-  min(7, 5)
+//   function min(a, b) {
+//     if (a < b) {
+//       return a;
+//     } else {
+//       return b;
+//     }
+//   }
+//   min(7, 5)
 
 
-  function pow(x, n) {
-    let result = x;
+//   function pow(x, n) {
+//     let result = x;
   
-    for (let i = 1; i < n; i++) {
-      result *= x;
-    }
+//     for (let i = 1; i < n; i++) {
+//       result *= x;
+//     }
   
-    return result;
-  }
+//     return result;
+//   }
   
-  let x = prompt("x?", '');
-  let n = prompt("n?", '');
+//   let x = prompt("x?", '');
+//   let n = prompt("n?", '');
   
-  if (n >= 1 && n % 1 == 0) {
-    alert( pow(x, n) );
-  } else {
-    alert(`Степень ${n} не поддерживается, используйте натуральное число`);
-  }
+//   if (n >= 1 && n % 1 == 0) {
+//     alert( pow(x, n) );
+//   } else {
+//     alert(`Степень ${n} не поддерживается, используйте натуральное число`);
+//   }
+
