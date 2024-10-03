a3505351cae5d86f9a1e9b7ffd6920d1c4c3dde7
emarishkin <ee.marishkin@yandex.ru>
2024-10-03 18:40:24 +0400

    constructor-new

--- Задачки js/zadacha.js
+++ Задачки js/zadacha.js
@@ -1,57 +1,57 @@
-// https://learn.javascript.ru/object-methods
-// Методы объекта, "this"
+// // https://learn.javascript.ru/object-methods
+// // Методы объекта, "this"
 
-// function showMessage() {
-//     alert( 'Всем привет!' );
-//   }
+// // function showMessage() {
+// //     alert( 'Всем привет!' );
+// //   }
   
-//   showMessage();
+// //   showMessage();
 
 
-//   function sum(a, b) {
-//     return a + b;
-//   }
+// //   function sum(a, b) {
+// //     return a + b;
+// //   }
   
-//   let result = sum(1, 2);
-//   alert( result ); // 3
+// //   let result = sum(1, 2);
+// //   alert( result ); // 3
 
-//   function checkAge(age) {
-//     if (age >= 18) {
-//       return true;
-//     } else {
-//       return confirm('А родители разрешили?');
-//     }
-//   }
+// //   function checkAge(age) {
+// //     if (age >= 18) {
+// //       return true;
+// //     } else {
+// //       return confirm('А родители разрешили?');
+// //     }
+// //   }
   
-//   let age = prompt('Сколько вам лет?', 18);
-//   if ( checkAge(age) ) {
-//     alert( 'Доступ получен' );
-//   }
-//     showMessage()     // показывает сообщение
-// getAge()          // возвращает возраст (получая его каким-то образом)
-// calcSum()         // вычисляет сумму и возвращает результат
-// createForm()      // создаёт форму (и обычно возвращает её)
-// checkPermission() // проверяет доступ, возвращая true/false
+// //   let age = prompt('Сколько вам лет?', 18);
+// //   if ( checkAge(age) ) {
+// //     alert( 'Доступ получен' );
+// //   }
+// //     showMessage()     // показывает сообщение
+// // getAge()          // возвращает возраст (получая его каким-то образом)
+// // calcSum()         // вычисляет сумму и возвращает результат
+// // createForm()      // создаёт форму (и обычно возвращает её)
+// // checkPermission() // проверяет доступ, возвращая true/false
 
 
-// function checkAge(age) {
-//     if (age > 18) {
-//       return true;
-//     } else {
-//       return confirm('Родители разрешили?');
-//     }
-//   }
-//   checkAge(20)
+// // function checkAge(age) {
+// //     if (age > 18) {
+// //       return true;
+// //     } else {
+// //       return confirm('Родители разрешили?');
+// //     }
+// //   }
+// //   checkAge(20)
 
 
-//   function min(a, b) {
-//     if (a < b) {
-//       return a;
-//     } else {
-//       return b;
-//     }
-//   }
-//   min(7, 5)
+// //   function min(a, b) {
+// //     if (a < b) {
+// //       return a;
+// //     } else {
+// //       return b;
+// //     }
+// //   }
+// //   min(7, 5)
 
 
 //   function pow(x, n) {
@@ -73,8 +73,8 @@
 //     alert(`Степень ${n} не поддерживается, используйте натуральное число`);
 //   }
 
-// https://learn.javascript.ru/object-methods
-// Методы объекта, "this"
+// // https://learn.javascript.ru/object-methods
+// //  Методы объекта, "this"
 
 // let user = {
 //     name: "John",
@@ -88,21 +88,80 @@
 //   user.sayHi(); // Привет!
 
   
-  let calculator = {
-    sum() {
-      return this.a + this.b;
-    },
+//   let calculator = {
+//     sum() {
+//       return this.a + this.b;
+//     },
   
-    mul() {
-      return this.a * this.b;
-    },
+//     mul() {
+//       return this.a * this.b;
+//     },
   
-    read() {
-      this.a = prompt('a?', 0);
-      this.b = prompt('b?', 0);
-    }
+//     read() {
+//       this.a = +prompt('a?', 0);
+//       this.b = +prompt('b?', 0);
+//     }
+//   };
+  
+//   calculator.read();
+//   alert( calculator.sum() );
+//   alert( calculator.mul() );
+
+
+//   let ladder = {
+//     step: 0,
+//     up() {
+//       this.step++;
+//       return this;
+//     },
+//     down() {
+//       this.step--;
+//       return this;
+//     },
+//     showStep() {
+//       alert( this.step );
+//       return this;
+//     }
+//   };
+
+//   ladder.up().up().down().showStep().down().showStep();
+
+// https://learn.javascript.ru/constructor-new
+// Конструктор, оператор "new"
+
+function Calculator() {
+
+  this.read = function() {
+    this.a = +prompt('a?', 0);
+    this.b = +prompt('b?', 0);
   };
-  
-  calculator.read();
-  alert( calculator.sum() );
-  alert( calculator.mul() );
+
+  this.sum = function() {
+    return this.a + this.b;
+  };
+
+  this.mul = function() {
+    return this.a * this.b;
+  };
+}
+
+let calculator = new Calculator();
+calculator.read();
+
+alert( "Sum=" + calculator.sum() );
+alert( "Mul=" + calculator.mul() );
+
+
+function Accumulator(startingValue) {
+  this.value = startingValue;
+
+  this.read = function() {
+    this.value += +prompt('Сколько нужно добавить?', 0);
+  };
+
+}
+
+let accumulator = new Accumulator(1);
+accumulator.read();
+accumulator.read();
+alert(accumulator.value);
